e8665f41c7a6e24034fe0241c859b11ee81be771
mohdhamzakhan <mohdhamzakhan@gmail.com>
2016-05-16 18:37:25 +0530

    Add files via upload

---
+++ Assigment3.java
@@ -0,0 +1,23 @@
+package Session2;
+
+public class Assigment3 {
+	public static void main(String[] args)
+	{
+			for(int j=1;j<=5;j++)
+			{
+				for(int k=1;k<=j;k++)
+			{
+					System.out.print(k);
+			}
+				System.out.println();
+		}
+			for(int i=4;i>=1;i--)
+			{
+				for(int h=1;h<=i;h++)
+				{
+					System.out.print(h);
+				}
+				System.out.println();
+			}
+	}
+}
